fb90a26723f55cb94d941b8dda12d44c43f4e536
Muizz-Aman <khanm@lawrence.edu>
2026-05-21 02:32:53 +0000

    tested sort_mutate. Doesn't fully work as intended but it's getting there

--- R/sort_mutate.R
+++ R/sort_mutate.R
@@ -1,3 +1,20 @@
+#' Applies sort_mutate
+#'
+#' @param .data dataframe
+#' @param expr expression, mathematical formula for your mutate
+#' @param desc automatically set to FALSE
+#'
+#' @returns a table with mutated new column
+#' @importFrom dplyr mutate arrange desc
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' shapes <- data.frame(length = c(10, 5, 8), width = c(2, 4, 5))
+#' area_shapes <- shapes |> sort_mutate(length*width, desc = TRUE)
+#' area_shapes
+#'
+#' # for now it doesn't show the new column, but it arranges according to the calculated area correctly
 sort_mutate <- function(.data, expr, desc = FALSE)
 {
   checkmate::assert_data_frame(.data)
