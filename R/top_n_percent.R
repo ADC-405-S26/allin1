8d9a4cb1894d63f580b777113d7ede2190569264
Muizz-Aman <khanm@lawrence.edu>
2026-05-25 19:03:47 +0000

    added top_n_percent

---
+++ R/top_n_percent.R
@@ -0,0 +1,22 @@
+#' Extract the top percentage of rows based on a column
+#'
+#' @param .data a dataframe
+#' @param col Column name to sort by
+#' @param percent number between 0 and 100 to specify the percentage
+#'
+#' @returns A filtered and sorted dataframe
+#' @importFrom dplyr arrange filter desc n
+#' @export
+#'
+#' @examples
+top_n_percent <- function(.data, col, percent) {
+  checkmate::assert_data_frame(.data)
+  checkmate::assert_number(percent, lower = 0, upper = 100)
+
+  # Calculate how many rows equal that percentage
+  num_rows <- max(1, round((percent / 100) * nrow(.data)))
+
+  .data |>
+    dplyr::arrange(dplyr::desc({{ col }})) |>
+    dplyr::filter(dplyr::row_number() <= num_rows)
+}
